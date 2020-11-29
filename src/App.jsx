f39ca57164a85cddedd58d0e1b449015dfc559ae
David Santacruz <dsanta87@gmail.com>
2020-11-29 20:24:35 +0100

    Set Numbers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
-import Button from './components/Button'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
+import Numbers from './components/Numbers'
 import './App.css'
 
 const App = () => {
-
-  const clickHandlerFunction = text => {
-    console.log('button clicked', text)
-  }
-
   return (
   <main className="react-calculator">
     <Result value={undefined} />
-    <div className="numbers">
-      <Button text="1" clickHandler={clickHandlerFunction}/>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>0</button>
-    </div>
+    <Numbers onClickNumber={number => {console.log('click on: ', number)}}/>
     <Functions
       onContentClear={clear => console.log('Clear: ', clear)}
       onDelete={onDelete => console.log('Delete: ', onDelete)}
