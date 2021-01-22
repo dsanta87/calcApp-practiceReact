b01672ca959d42040b0a1be0c37a0a909cf2a220
David Santacruz <dsanta87@gmail.com>
2021-01-22 09:06:46 +0100

    Add lodash.words

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+/* eslint no-eval: 0 */
 import React, {useState} from 'react'
+import words from 'lodash.words'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
@@ -9,20 +11,39 @@ const App = () => {
 
   const [stack, setStack] = useState('')
 
+  const items = words(stack, /[^-^+^*^/]+/g)
+
+  console.log('items', items)
+
   return (
   <main className="react-calculator">
-    <Result value={stack} />
+    <Result value={items[items.length-1]} />
     <Numbers onClickNumber={number => {
       console.log('click on: ', number)
       setStack(`${stack}${number}`)
       }}/>
     <Functions
-      onContentClear={clear => console.log('Clear: ', clear)}
-      onDelete={onDelete => console.log('Delete: ', onDelete)}
+      onContentClear={clear => {
+        console.log('Clear: ', clear)
+        setStack('')
+      }}
+      onDelete={onDelete => {
+        if(stack.length > 0) {
+          console.log('Delete: ', onDelete)
+          const newStack = stack.substring(0, stack.length - 1)
+          setStack(newStack)
+        }
+      }}
     />
     <MathOperations
-      onClickOperation={operation => console.log('Operation: ', operation)}
-      onClickEqual={equal => console.log('Equal: ', equal)}
+      onClickOperation={operation => {
+        console.log('Operation: ', operation)
+        setStack(`${stack}${operation}`)
+      }}
+      onClickEqual={equal => {
+        console.log('Equal: ', equal)
+        setStack(eval(stack).toString())
+      }}
     />
   </main>)
 }
